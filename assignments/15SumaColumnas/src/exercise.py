7099755993b936817183904e0f8fad0107b6f48f
GasparNavarroMoreno <a01252422@itesm.mx>
2021-10-15 18:02:00 +0000

    examenParte2Gaspar

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,7 +1,27 @@
-
+def crearmatriz():
+    Lista = []
+    Fila = int(input())
+    Columna = int(input())
+    if Fila > 0 and Columna > 0:
+        for i in range(Fila):
+            Lista.append([])
+            for y in range(Columna):
+                x = int(input())
+                Lista[i].append(x)
+    else:
+        print('Error')
+    return Lista
 
 def main():
-
+    Matriz = crearmatriz()
+    Lista_col = []
+    if len(Matriz) > 0:
+        for i in range(len(Matriz[0])):
+            count = 0
+            for y in range(len(Matriz)):
+                count += Matriz[y][i]
+            Lista_col.append(count)
+        print(Lista_col)
 
 
 if __name__=='__main__':
